b31e9180d3301e230990a812996b8b768948d2e0
carlosperate <carlosperate@users.noreply.github.com>
2019-08-19 00:18:45 +0100

    Add basic BLE UART capability.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,58 @@
 (function() {
 'use strict';
 
-function dummyClickHandler(e) {
-  console.log('click');
-  navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
-  if (navigator.vibrate) {
-    window.navigator.vibrate(25);
-  }
+navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
+var microbitUart = null;
+
+function log(msg, obj) {
+    console.log(msg, obj || '');
 }
 
-$('#controller-button-up').click(dummyClickHandler);
-$('#controller-button-down').click(dummyClickHandler);
-$('#controller-button-left').click(dummyClickHandler);
-$('#controller-button-right').click(dummyClickHandler);
-$('#controller-button-centre').click(dummyClickHandler);
-$('#controller-button-a').click(dummyClickHandler);
-$('#controller-button-b').click(dummyClickHandler);
-$('#controller-button-start').click(dummyClickHandler);
-$('#controller-button-select').click(dummyClickHandler);
+$('#connect').click(function() {
+    microbit.requestMicrobit(window.navigator.bluetooth).then(function(device) {
+        return microbit.getServices(device);
+    }).then(function(services) {
+        if (services.uartService) {
+            microbitUart = services.uartService;
+            services.uartService.addEventListener('receiveText', function(msg) {
+                log('UART received: ' + msg);
+            });
+        }
+        if (services.deviceInformationService) {
+            return services.deviceInformationService.readDeviceInformation();
+        } else {
+            return new Promise(function(resolve, reject) {
+                resolve('No device info.');
+            });
+        }
+    }).then(function(info) {
+        log('Ready: ', info);
+    }).catch(function(err) {
+        log('Connection Error: ', err.msg);
+    });
+});
+
+function sendCommand(cmd) {
+    log('Command: ' + cmd);
+    if (microbitUart) {
+        microbitUart.send(new TextEncoder().encode(cmd + '\n'));
+    } else {
+        log('UART not yet available.');
+    }
+    if (navigator.vibrate) {
+        window.navigator.vibrate(25);
+    }
+}
+
+$('#controller-button-up').click(() => sendCommand('UP'));
+$('#controller-button-down').click(() => sendCommand('DOWN'));
+$('#controller-button-left').click(() => sendCommand('LEFT'));
+$('#controller-button-right').click(() => sendCommand('RIGHT'));
+$('#controller-button-centre').click(() => sendCommand('CENTRE'));
+$('#controller-button-a').click(() => sendCommand('A'));
+$('#controller-button-b').click(() => sendCommand('B'));
+$('#controller-button-start').click(() => sendCommand('START'));
+$('#controller-button-select').click(() => sendCommand('SELECT'));
 
 window.scrollTo(0,1);
 })();
